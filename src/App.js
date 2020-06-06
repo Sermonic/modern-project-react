b743701df52084accfbba08c8685168307aaaa11
Sermonic <vitaliyshcherbanych@gmail.com>
2020-06-06 12:02:49 +0300

    Hot Reloading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
-export const App = () => (
+const App = () => (
   <div className="App">
     <h1>Hello, World!</h1>
   </div>
 );
+
+export default hot(module)(App);
