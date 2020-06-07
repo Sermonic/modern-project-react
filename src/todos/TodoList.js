bb8c4a4595155b020ec1d009fd0f5ca8a6bedaad
Sermonic <vitaliyshcherbanych@gmail.com>
2020-06-07 12:43:52 +0300

    Putting Components Together

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,11 +1,15 @@
 import React from "react";
-import TodoListItem from "./TodoListItem";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoListItem } from "./TodoListItem";
 import "./TodoList.css";
 
-export const TodoList = ({ todos }) => {
-  <div className="list-wrapper">
-    {todos.map((todo) => (
-      <TodoListItem todo={todo} />
-    ))}
-  </div>;
+export const TodoList = ({ todos = [{ text: "Hello" }] }) => {
+  return (
+    <div className="list-wrapper">
+      <NewTodoForm />
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} />
+      ))}
+    </div>
+  );
 };
