0badfc8e454a893b834a9453a1fdfade553f69c2
Sermonic <vitaliyshcherbanych@gmail.com>
2020-06-07 13:30:24 +0300

    Create Reducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore, combineRedicers } from "redux";
+import { todos } from "./todos/reducers";
 
-const reducers = {};
+const reducers = {
+  todos,
+};
 
 const rootReducer = combineRedicers(reducers);
 
