988c30013c5cff9897656b77ae463158140d905b
Sermonic <vitaliyshcherbanych@gmail.com>
2020-06-12 14:17:59 +0300

    Adding selector

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import NewTodoForm from "./NewTodoForm";
 import { TodoListItem } from "./TodoListItem";
+import { getTodos, getTodosLoading } from "./selectors";
 import {
   loadTodos,
   removeTodoRequest,
@@ -39,8 +40,8 @@ const TodoList = ({
 };
 
 const mapStateToProps = (state) => ({
-  isLoading: state.isLoading,
-  todos: state.todos,
+  isLoading: getTodosLoading(state),
+  todos: getTodos(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
